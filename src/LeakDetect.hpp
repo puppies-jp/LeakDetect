11564b8c9a20309c6c422dc9f74d4c2c31ec5f60
puppies <37975692+puppies-jp@users.noreply.github.com>
2021-12-20 21:02:03 +0900

    delete header file

--- src/LeakDetect.hpp
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <iostream>
-#include <typeinfo>
-#include <tuple>
-#include <map>
-
-#define STRINGIFY(n) #n
-#define TOSTRING(n) STRINGIFY(n)
-#define PREFIX __FILE__ ":" TOSTRING(__LINE__) ": "
--- src/test.cpp
+++ src/test.cpp
@@ -1,5 +1,12 @@
-#include "LeakDetect.hpp"
-#include <unistd.h>
+#include <stdio.h>
+#include <iostream>
+#include <typeinfo>
+#include <tuple>
+#include <map>
+
+#define STRINGIFY(n) #n
+#define TOSTRING(n) STRINGIFY(n)
+#define PREFIX __FILE__ ":" TOSTRING(__LINE__) ": "
 
 class test
 {
